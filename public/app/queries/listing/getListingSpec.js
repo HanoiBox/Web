c939553a6606057cc5013ecb0bdcf992df6bed3d
garethfentimen <garethfentimen@gmail.com>
2016-06-29 23:05:37 +0100

    unit test tech debt

---
+++ public/app/queries/listing/getListingSpec.js
@@ -0,0 +1,5 @@
+import angular from 'angular';
+import 'angular-mocks';
+import lowestCategoriesCommandModule from './getListings';
+
+//TODO unit test to guarantee that listings are returned from object if successful
