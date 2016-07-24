a0a3c9c1485d69fed63a82595033f8cb7005784d
garethfentimen <garethfentimen@gmail.com>
2016-07-24 16:34:40 +0100

    Front end unit tests running, now need to fix

--- karma.conf.js
+++ karma.conf.js
@@ -1,45 +1,31 @@
 // Karma configuration
-// Generated on Sat Nov 21 2015 15:07:02 GMT+0000 (GMT Standard Time)
+// Generated on Sun Jul 24 2016 15:22:50 GMT+0100 (GMT Summer Time)
 
 module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-    
+
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    //Chrome
-    browsers: ['PhantomJS'],
-    
+    browsers: ['Chrome', 'PhantomJS'],
+
     plugins: [
             'karma-jspm',
             'karma-jasmine',
             'karma-chrome-launcher',
-            'karma-phantomjs-launcher'
-        ],
+            'karma-phantomjs-launcher'],
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jspm', 'jasmine'],
 
-    //list of files / patterns to load in the browser
+
+    // list of files / patterns to load in the browser
     files: [
-      //'node_modules/babel-polyfill/dist/polyfill.js'
-      //{pattern: '*Spec.js', included: false},
-      // {pattern: 'sysadmin/*Spec.js', included: false},
-      // {pattern: 'sysadmin/**/*Spec.js', included: false},
-      //{pattern: '**/*Spec.js', included: false}
     ],
-    
-    // proxies: {
-    //     '/base/': '/base/public/app/commands/category/'
-    // },
-    
-    // paths: {
-    //     '/base': 'public/*.js'
-    // },
-    
+
     jspm: {
         // NB. tests or controllers which exist in both areas with the same name
         // causes serious confusion and grief for me and my Karma GF 16/01/2016
@@ -55,6 +41,7 @@ module.exports = function(config) {
     // list of files to exclude
     exclude: [
     ],
+
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
@@ -82,16 +69,17 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    autoWatch: false,
+
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
 
     // Concurrency level
-    // how many browser should be started simultanous
+    // how many browser should be started simultaneous
     concurrency: Infinity,
-    
+
     phantomjsLauncher: {
       // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom) 
       exitOnResourceError: true
--- test.ps1
+++ test.ps1
@@ -1,4 +1,4 @@
 node node_modules/jasmine/bin/jasmine.js
 #npm install -g karma-cli
-#karma start my.conf.js --single-run
+#karma start karma.conf.js --single-run
 #--log-level debug
